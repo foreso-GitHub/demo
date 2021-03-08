f1b8829375cc74a39348294717e7bf56dad05967
foreso <foreso@sina.com>
2021-03-08 17:46:13 +0800

    增加一些发现到文档

--- doc/demo链初始化注意事项.md
+++ doc/demo链初始化注意事项.md
@@ -1,3 +1,6 @@
+## 配置文件
+* 格式
+```
 {
     "address":"localhost",
     "port":7001,
@@ -14,3 +17,8 @@
         "localhost:7005"
     ]
 }
+```
+* 注意
+    * 每个节点的gas_address必须一致
+    * 每个节点的secret必须不一致
+    * 目前账本的地址格式是使用以太坊的地址格式（比如0xc287B1266732495Fe8c93CE3Ba631597153fdd91）， 而非井通的格式（比如jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh），可以通过插拔账本模块来改变
---
+++ doc/reset.bat
@@ -0,0 +1,14 @@
+rd /s/q "C:\Users\xuou\go\src\github.com\tokentransfer\demo\data"
+rd /s/q "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node1\data"
+rd /s/q "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node2\data"
+rd /s/q "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node3\data"
+rd /s/q "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node4\data"
+rd /s/q "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node5\data"
+
+copy /Y "C:\Users\xuou\go\src\github.com\tokentransfer\demo\main.exe" "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node1\main.exe"
+copy /Y "C:\Users\xuou\go\src\github.com\tokentransfer\demo\main.exe" "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node2\main.exe"
+copy /Y "C:\Users\xuou\go\src\github.com\tokentransfer\demo\main.exe" "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node3\main.exe"
+copy /Y "C:\Users\xuou\go\src\github.com\tokentransfer\demo\main.exe" "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node4\main.exe"
+copy /Y "C:\Users\xuou\go\src\github.com\tokentransfer\demo\main.exe" "E:\2. work\井系\3. 链景\井通新链\demo链\demo\node5\main.exe"
+
+
